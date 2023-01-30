35716c889153ad239db628684f1844ac6057791a
Joseph-anselm <iamjoeanselm@gmail.com>
2023-01-30 16:36:18 +0100

    menu fix

---
+++ components/menu.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar collapseOnSelect scroling dark expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home">
+            <Image
+              src="/../public/images/logo2.png"
+              width="120"
+              height="58"
+              className="d-inline-block align-top"
+              alt="Oluwaseun Anselm logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto justify-center">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About Me</Nav.Link>
+              <Nav.Link href="#pricing">Showcase</Nav.Link>
+              <FontAwesomeIcon icon={"coffee"} />
+
+              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown> */}
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets" className="menubar">
+                <i className="bi bi-facebook"></i>
+                <i className="bi bi-linkedin"></i>
+                <i className="bi bi-youtube"></i>
+              </Nav.Link>
+              <FontAwesomeIcon icon={"user-secret"} />
+              <Nav.Link eventKey={2} href="#memes">
+                <i className="bi bi-share"></i>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default ColorSchemesExample;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "@/components/layouts";
+import RootLayout from "../components/layouts";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <RootLayout>
+        <Component {...pageProps} />;
+      </RootLayout>
+    </div>
+  );
 }
