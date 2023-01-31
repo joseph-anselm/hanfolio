8a519f9ace1cc34176471fe42a3f3a6cea36d5a4
Joseph-anselm <iamjoeanselm@gmail.com>
2023-01-31 14:15:59 +0100

    menu fix

--- components/menu.js
+++ components/menu.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 function ColorSchemesExample() {
+  // const { navbar, setNavbar } = useState(false);
+  // const changebackground = () => {
+  //   if (window.scrollY >= 30) {
+  //     setNavbar;
+  //   } else {
+  //     setNavbar;
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", changebackground);
+  //   return () => window.removeEventListener("scroll", changebackground);
+  // });
+
+  useEffect(() => {
+    // The debounce function receives our function as a parameter
+    const debounce = (fn) => {
+      // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+      let frame;
+      // The debounce function returns a new function that can receive a variable number of arguments
+      return (...params) => {
+        // If the frame variable has been defined, clear it now, and queue for next frame
+        if (frame) {
+          cancelAnimationFrame(frame);
+        }
+        // Queue our function call for the next frame
+        frame = requestAnimationFrame(() => {
+          // Call our function and pass any params we received
+          fn(...params);
+        });
+      };
+    };
+
+    // Reads out the scroll position and stores it in the data attribute
+    // so we can use it in our stylesheets
+    const storeScroll = () => {
+      document.documentElement.dataset.scroll = window.scrollY;
+    };
+
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener("scroll", debounce(storeScroll), {
+      passive: true,
+    });
+
+    // Update scroll position for first time
+    storeScroll();
+  });
+
   return (
     <>
       <Container className={styles.menutop}>
