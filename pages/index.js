f6ac283afad8bf3de5849f137eba5faf3bb4176d
Joseph-anselm <iamjoeanselm@gmail.com>
2023-02-05 15:23:56 +0100

    section  fix

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import {
-  FaUsd,
-  FaLaptop,
-  FaPencilAlt,
-  FaCloud,
-  FaUserEdit,
-} from "react-icons/fa";
+import { FaLaptop, FaPencilAlt, FaCloud, FaUserEdit } from "react-icons/fa";
 
 const inter = Inter({ subsets: ["latin"] });
 
